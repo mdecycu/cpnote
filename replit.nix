df15cc5b9f15a95f2d7d6b532a16c4b94b4f2503
mdecycu <101738344+mdecycu@users.noreply.github.com>
2023-12-12 06:24:42 +0000

    加入 w14 1a 的兩個說明影片, 包括統整 c +gd 以及開始使用 Gist 與 Brython 執行程式

--- replit.nix
+++ replit.nix
@@ -1,9 +1,15 @@
 { pkgs }: {
     deps = [
+      pkgs.libev
       pkgs.gnuplot
       pkgs.ncurses.dev
       pkgs.gd
       pkgs.vimHugeX
       pkgs.raylib
     ];
+  env = {
+    PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+      pkgs.libev
+    ];
+  };
 }
